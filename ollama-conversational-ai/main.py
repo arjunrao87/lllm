5fcd4ac57f53d5d73eb016bee9bfa3ac8de07d69
Arjun Rao <rao.arjun87@gmail.com>
2024-03-07 22:20:25 -0500

    fix: syntax

--- ollama-conversational-ai/main.py
+++ ollama-conversational-ai/main.py
@@ -1,16 +1,14 @@
 from langchain_community.llms import Ollama
 from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import Runnable
 
 import chainlit as cl
 
 
 @cl.on_chat_start
 async def on_chat_start():
-    # Sending an image with the local file path
     elements = [cl.Image(name="image1", display="inline", path="assets/gemma.jpeg")]
     await cl.Message(
-        content="Hello there, I am Gemma. How can I help you ?", elements=elements
+        content="Hello there, I am Gemma. How can I help you?", elements=elements
     ).send()
     model = Ollama(model="mistral")
     prompt = ChatPromptTemplate.from_messages(
@@ -22,18 +20,18 @@ async def on_chat_start():
             ("human", "{question}"),
         ]
     )
-    runnable = prompt | model
-    cl.user_session.set("runnable", runnable)
+    chain = prompt | model
+    cl.user_session.set("chain", chain)
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
 
-    runnable = cl.user_session.get("runnable")  # type: Runnable
+    chain = cl.user_session.get("chain")
 
     msg = cl.Message(content="")
 
-    async for chunk in runnable.astream(
+    async for chunk in chain.astream(
         {"question": message.content},
     ):
         await msg.stream_token(chunk)
